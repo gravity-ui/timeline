082bd2991b9ceac4d548d1c4e75332b360600b7a
simbiozizv <simbioziz@gmail.com>
2025-05-21 17:34:05 +0300

    chore(Config): replace values

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -1,32 +1,31 @@
 import { ViewConfigurationDefault } from "../types/configuration";
-import { yaTimelineConfig } from "../config";
 
 export const defaultViewConfig: ViewConfigurationDefault = {
   hideRuler: false,
   ruler: {
-    spacing: yaTimelineConfig.RULER_LABEL_SPACING,
-    position: yaTimelineConfig.RULER_LABEL_POS,
-    subPosition: yaTimelineConfig.RULER_SUP_LABEL_POS,
-    height: yaTimelineConfig.RULER_HEADER_HEIGHT,
-    font: yaTimelineConfig.RULER_FONT,
+    spacing: 40,
+    position: 33,
+    subPosition: 16,
+    height: 41,
+    font: "10px sans-serif",
     color: {
-      background: yaTimelineConfig.PRIMARY_BACKGROUND_COLOR,
-      primaryLevel: yaTimelineConfig.RULER_PRIMARY_TEXT_COLOR,
-      secondaryLevel: yaTimelineConfig.RULER_SECONDARY_TEXT_COLOR,
-      textOutlineColor: yaTimelineConfig.RULER_TEXT_OUTLINE_COLOR,
-      borderColor: yaTimelineConfig.RULER_BORDER_COLOR,
+      background: "#FFF",
+      primaryLevel: "#111",
+      secondaryLevel: "#777",
+      textOutlineColor: "#FFF",
+      borderColor: "#333",
     },
   },
   grid: {
-    spacing: yaTimelineConfig.RULER_LABEL_SPACING,
-    lineWidth: yaTimelineConfig.GRID_STROKE_WIDTH,
+    spacing: 40,
+    lineWidth: 1,
     widthBuffer: 40,
   },
   axes: {
-    trackHeight: yaTimelineConfig.TRACK_HEIGHT,
-    lineHeight: yaTimelineConfig.LINE_HEIGHT,
+    trackHeight: 25,
+    lineHeight: 25,
     color: {
-      line: yaTimelineConfig.SECONDARY_MARK_COLOR,
+      line: "#DDD",
     },
     lineWidth: 1,
     dashedLinePattern: [5, 3],
