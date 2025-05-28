124244c02a1686eea9708608cb62b7dbcf91716b
simbiozizv <simbioziz@gmail.com>
2025-05-29 00:19:14 +0300

    chore(UI): removed unused colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -4,11 +4,5 @@ export const defaultPalette = {
   gray7: "#777",
   gray13: "#DDD",
   white: "#FFF",
-  blue: "#08F",
-  blue10: "#8BF",
-  green: "#3D5",
   red: "#D30",
-  gold: "#F80",
-  yellow: "#fe5",
-  purple: "#A5A",
 };
