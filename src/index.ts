149d241aada9e7d38d5d483ad21d670e0554ac22
simbiozizv <simbioziz@gmail.com>
2025-07-24 20:24:15 +0300

    feat(UI): split export to js and react components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-export { Timeline } from "./Timeline";
-export { AbstractEventRenderer } from "./components/Events/AbstractEventRenderer";
-export { defaultViewConfig } from "./constants/options";
+export * from "./Timeline";
+export * from "./components/Events/AbstractEventRenderer";
+export * from "./constants/options";
 export * from "./helpers/math";
 export * from "./types";
 export * from "./enums";
-export { TimelineCanvas } from "./react-component/TimelineCanvas";
-export { useTimeline } from "./react-component/hooks/useTimeline";
-export { useTimelineEvent } from "./react-component/hooks/useTimelineEvent";
---
+++ src/react-component/index.ts
@@ -0,0 +1,2 @@
+export * from "./TimelineCanvas";
+export * from "./hooks";
