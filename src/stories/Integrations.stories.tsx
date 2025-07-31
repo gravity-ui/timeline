c77992b158ec2b9879f8c117e55244fd6c8d220c
simbiozizv <simbioziz@gmail.com>
2025-08-01 01:05:06 +0300

    feat(storybook): add integration range date selection example

---
+++ src/stories/Integrations.stories.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { RangeSelection } from "./integrations/RangeSelection";
+
+const meta = {
+  title: "Timeline/Integrations",
+  component: null,
+  decorators: [
+    (Story) => (
+      <div style={{ width: "100%", height: "400px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const RangeDateSelectionStory: Story = {
+  name: "RangeDateSelection",
+  render: () => <RangeSelection />,
+  parameters: {
+    docs: {
+      description: {
+        story: "RangeDateSelection component from integrations",
+      },
+    },
+  },
+};
