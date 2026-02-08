0dfaa0836707cd5c3e75711b8996644849220357
Antamansid <Antamansid@mail.ru>
2026-02-08 14:01:08 +0300

    feat: export AbstractSectionRenderer for custom section renderers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./Timeline";
 export * from "./components/Events/AbstractEventRenderer";
 export * from "./components/Markers/AbstractMarkerRenderer";
+export * from "./components/Sections/AbstractSectionRenderer";
 export * from "./constants/options";
 export * from "./helpers/math";
 export * from "./types";
