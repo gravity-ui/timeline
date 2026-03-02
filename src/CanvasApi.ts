5e7b9d64529f0d8300bf97c39bf60f3d9198dfc8
Antamansid <antamansid@yandex.ru>
2026-03-02 17:59:31 +0300

    fix: reset canvas transform before clearing to prevent rendering artifacts

--- src/CanvasApi.ts
+++ src/CanvasApi.ts
@@ -202,6 +202,7 @@ export class CanvasApi<
   }
 
   public clear(): void {
+    this.ctx.setTransform(1, 0, 0, 1, 0, 0);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
